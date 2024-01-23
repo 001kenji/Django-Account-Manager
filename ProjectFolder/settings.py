4dbc6adb19c65f10f4baccca7ca124e0361dd0e2
001kenji <briannjuguna694@gmail.com>
2024-01-23 13:59:45 +0300

    vercel config

--- ProjectFolder/settings.py
+++ ProjectFolder/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5j&36%uf$h-h(^-@#po6n)b_n5r6zi&9bh^(9xfpd^39of#6*w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
