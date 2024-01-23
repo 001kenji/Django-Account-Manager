abdb1db830ddd61877def2e6a8d49dc15a8fbcea
001kenji <briannjuguna694@gmail.com>
2024-01-23 15:19:45 +0300

    change

--- ProjectFolder/settings.py
+++ ProjectFolder/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-5j&36%uf$h-h(^-@#po6n)b_n5r6zi&9bh^(9xfpd^39of#6*w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['.vercel.app']
-ALLOWED_HOSTS = ['*']
+ ALLOWED_HOSTS = ['.vercel.app']
+
 
 # Application definition
 
